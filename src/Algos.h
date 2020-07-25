cf7282e69b470556923df2cb360461bf8db84dc3
Nitish Shingde <ngshingde@gmail.com>
2020-07-25 16:18:57 +0530

    Add bfs algorithm
    - add bfs function
    - add unit test for the implemented bfs function

--- src/Algos.h
+++ src/Algos.h
@@ -4,6 +4,7 @@
 
 #include "Graph.h"
 #include <vector>
+#include <deque>
 
 template <typename Edge>
 std::vector<std::int32_t> dfs(const Graph<Edge> &graph, const std::int32_t &node) {
@@ -30,6 +31,30 @@ std::vector<std::int32_t> dfs(const Graph<Edge> &graph, const std::int32_t &node
     return traversalPath;
 }
 
+template<typename Edge>
+std::vector<std::int32_t> bfs(const Graph<Edge> &graph, const std::int32_t &node) {
+    std::vector<std::int32_t> traversalPath;
+    traversalPath.reserve(graph.size());
+    std::vector<bool> isVisited(graph.mDataStore.size());
+    std::deque<std::int32_t> queue;
+    auto processNode = [&traversalPath, &isVisited, &queue](const std::int32_t &node) {
+        traversalPath.emplace_back(node);
+        isVisited[node] = true;
+        queue.emplace_back(node);
+    };
+
+    //core algorithm
+    processNode(node);
+    for(; !queue.empty(); queue.pop_front()) {
+        for(auto &neighbour: graph.getNeighbours(queue.front())) {
+            if(!isVisited[neighbour.getIndex()]) {
+                processNode(neighbour.getIndex());
+            }
+        }
+    }
+
+    return traversalPath;
+}
 
 
 #endif //CPP_GRAPH_ALGOS_H
--- unit_test/Test_algos.cpp
+++ unit_test/Test_algos.cpp
@@ -11,6 +11,7 @@ TEST_CASE("Test Depth First Search algorithm", "[dfs]") {
     graph[2].emplace_back(4);
     graph[3].emplace_back(5);
     graph[3].emplace_back(6);
+    graph[5].emplace_back(6);
 
     std::string path;
     for(auto index :dfs(graph, 1)) {
@@ -18,8 +19,37 @@ TEST_CASE("Test Depth First Search algorithm", "[dfs]") {
     }
     CHECK((
         path == "124356"
-        or path == "123465"
+        or path == "124365"
         or path == "135624"
         or path == "136524"
+    ));
+}
+
+TEST_CASE("Test Breadth First Search algorithm", "[bfs]") {
+    AdjacencyList graph(7);
+    graph[1].emplace_back(2);
+    graph[1].emplace_back(3);
+    graph[2].emplace_back(4);
+    graph[3].emplace_back(5);
+    graph[3].emplace_back(6);
+    graph[5].emplace_back(6);
+
+    std::string path;
+    for(auto index :bfs(graph, 1)) {
+        path += std::to_string(index);
+    }
+    CHECK((
+        path == "123456"
+        or path == "123465"
+        or path == "123546"
+        or path == "123564"
+        or path == "123645"
+        or path == "123654"
+        or path == "132456"
+        or path == "132465"
+        or path == "132546"
+        or path == "132564"
+        or path == "132645"
+        or path == "132654"
     ));
 }
